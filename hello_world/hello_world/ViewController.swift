646149f524d23b165b3563b3bc76ce08711e35ce
Pham Anh Tuan <tuanpham5024@gmail.com>
2023-05-18 15:08:06 +0700

    inital commit

---
+++ hello_world/hello_world/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello_world
+//
+//  Created by Tuấn Phạm on 18/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
